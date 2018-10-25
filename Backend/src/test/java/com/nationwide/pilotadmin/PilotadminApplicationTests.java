b326015aaba3455ddcdac70a2493b75fc079e044
rushidesai <rushidesai1@gmail.com>
2018-10-25 18:42:44 -0500

    adding ide related files

---
+++ Backend/src/test/java/com/nationwide/pilotadmin/PilotadminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nationwide.pilotadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PilotadminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
